2215a1f71aeb3653689f246d16207c484ba02187
Vishnu Rangiah <vrrangiah@ucdavis.edu>
2020-03-28 15:31:27 -0700

    base ui/ux coded

--- src/components/ConnectH.js
+++ src/components/ConnectH.js
@@ -2,11 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
 
+
+import SendIcon from '@material-ui/icons/Send';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 
 const Styles = styled.div`
@@ -38,20 +47,11 @@ const Himg = styled.img`
  const Subtitle = styled.h3`
      font-family: IBM Plex Sans;
      font-style: normal;
-     font-size: 1.75rem;
+     font-size: 1.55rem;
      color: #000;
-     padding-top: 3.25rem;
-     padding-right: 2rem;
-     padding-left: 2rem;
-     padding-bottom: 1.5rem;
+     padding: 1.75rem 2rem;
+
      letter-spacing: 0.02rem;
-
-     .email{
-
-       padding-bottom:0rem;
-     }
-
-
  `;
 
 
@@ -61,7 +61,7 @@ const Himg = styled.img`
       text-align: center;
     }
 
-    .headbutton{
+    .headbutton2{
       color: white;
       background: linear-gradient(99.56deg, #001C84 3.37%, #BC5555 89.07%);;
       font-family: IBM Plex Sans;
@@ -69,6 +69,9 @@ const Himg = styled.img`
       font-size: .9rem;
       font-weight: bold;
       letter-spacing: 0.1rem;
+      margin-top: .4rem;
+      padding: .5rem;
+      margin-left: .75rem;
 
       &:hover {
         background-color: black;
@@ -79,12 +82,43 @@ const Himg = styled.img`
     .MuiInputLabel-outlined,.MuiFormHelperText-contained, .MuiOutlinedInput-input{
       font-family: IBM Plex Sans;
       font-style: normal;
+      font-size:1rem;
 
     }
 
     .MuiFormHelperText-root{
       margin-top: .45rem;
     }
+
+    .MuiOutlinedInput-input{
+      padding-top: 1.15rem;
+      padding-bottom: 1.15rem;
+      padding-right: 5rem;
+      padding-left: .65rem;
+    }
+
+    .email{
+      padding-bottom: 1.75rem;
+    }
+
+
+
+    .MuiFormControl-marginNormal{
+      margin-top:0rem;
+    }
+
+    .buttongroup{
+      padding-top:.25rem;
+
+    }
+
+    .iconz{
+      font-size:2rem;
+
+    }
+
+
+
 
   `;
 
@@ -105,6 +139,36 @@ export const ConnectH = () => (
                 <Subtitle>
                         Please reach out to us for any information
                          and connect with us on social media.</Subtitle>
+
+
+
+                  <ButtonGroup size="large" className="buttongroup" aria-label="large outlined primary button group">
+
+                      <IconButton  aria-label="Facebook">
+                       <FacebookIcon className="iconz" />
+                       </IconButton>
+
+                       <IconButton aria-label="Github">
+                        <GitHubIcon className="iconz"/>
+                        </IconButton>
+
+                        <IconButton aria-label="LinkedIn">
+                         <LinkedInIcon className="iconz"/>
+                         </IconButton>
+
+                         <IconButton aria-label="Email">
+                          <MailOutlineIcon className="iconz"/>
+                          </IconButton>
+
+
+                      <IconButton aria-label="Youtube">
+                       <YouTubeIcon className="iconz"/>
+                       </IconButton>
+
+
+                   </ButtonGroup>
+
+
                 </Grid>
 
                 <Grid  className="righthead" item sm={4}>
@@ -117,6 +181,8 @@ export const ConnectH = () => (
 
                 <Subtitle className="email" >Join our mailing list.</Subtitle>
 
+                      <div style = {{display: "inline-block"}}>
+
                 <TextField
                      id="outlined-basic"
                      label="Enter Email"
@@ -124,12 +190,19 @@ export const ConnectH = () => (
                      margin="normal"
                      variant="outlined"  />
 
-                     <Button
-                       variant="contained"
-                       color="primary">
 
-                       Send
+
+                     <Button
+                       className = "headbutton2"
+                       variant="contained"
+                       endIcon={<SendIcon />}> Send
                      </Button>
+
+                     </div>
+
+                     <br />
+
+
 
 
                 </Grid>
