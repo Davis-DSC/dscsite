fa6c93cb6b69d80b056d82c15c59973c467187c1
Davis-DSC <vrrangiah@ucdavis.edu>
2022-11-06 01:39:34 -0400

    added event

---
+++ src/components/Event.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+const Style = styled.div`
+
+`;
+
+const Cals = styled.div`
+
+`;
+
+const Subtitle = styled.h3`
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 1.5rem;
+  color: #000;
+  padding-top: 2.75rem;
+  padding-bottom: 1.5rem;
+  letter-spacing: 0.02rem;
+
+  .grade {
+    color: #3846c6;
+  }
+`;
+
+
+export const Event = () => (
+   <div>
+    
+
+   </div>
+)
